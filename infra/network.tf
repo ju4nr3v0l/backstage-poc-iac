15f2bc808c65f5f0d83605a335ec55b028dfd8a4
ju4nr3v0l <juandavidmarulanda@yahoo.com>
2025-05-09 10:16:14 -0500

    network delegation fix autodelegate

--- infra/network.tf
+++ infra/network.tf
@@ -15,17 +15,5 @@ resource "azurerm_subnet" "aks" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
-
-  delegation {
-    name = "aks_delegation"
-    service_delegation {
-      name = "Microsoft.ContainerService/managedClusters"
-      actions = [
-        "Microsoft.Network/virtualNetworks/subnets/join/action",
-        "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action",
-        "Microsoft.Network/virtualNetworks/subnets/unprepareNetworkPolicies/action",
-      ]
-    }
-  }
 }
 
