364c6c744d2082b41f9c382d7e2294e7e9866f23
ju4nr3v0l <juandavidmarulanda@yahoo.com>
2025-05-09 16:15:01 -0500

    add argoCd

--- infra/argocd/argocd.tf
+++ infra/argocd/argocd.tf
@@ -1,66 +1,28 @@
-#############################################
-# 1. Namespace Argo CD
-#############################################
-resource "kubernetes_namespace" "argocd" {
-  metadata {
-    name = "argocd"
-  }
-}
-
-#############################################
-# 2. Helm Chart Argo CD
-#############################################
+##################################
+# Helm release Argo CD simplificada
+##################################
 resource "helm_release" "argocd" {
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
-  version          = "5.46.6"                     # estable a mayo-2025
-  namespace        = kubernetes_namespace.argocd.metadata[0].name
-  create_namespace = false
+  version          = "5.46.6"
+  namespace        = "argocd"
+  create_namespace = true         # Helm crea el NS
 
-  values = [
-    yamlencode({
-      installCRDs = true
-      server = {
-        service = {
-          type = "LoadBalancer"
-          annotations = {
-            # crea una IP pública con nombre fijo en el node RG
-            "service.beta.kubernetes.io/azure-pip-name" = "argocd-server-pip"
-          }
-        }
-        ingress = { enabled = false }            # aún sin DNS
-      }
-      configs = {
-        secret = {
-          # password admin bcryptada → "Admin123!"
-          argocdServerAdminPassword = "$2a$10$HWsa3A9XyDYt2y57Q7LyvOrOAJr2MJbWxfNuQmzgOVLC6muX19e6m"
-        }
-      }
-    })
-  ]
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
 
-  timeout    = 600
-  depends_on = [helm_release.nginx_ingress]       # ya tienes NGINX instalado
-}
+  # Exponer con LoadBalancer (sin DNS todavía)
+  set {
+    name  = "server.service.type"
+    value = "LoadBalancer"
+  }
+  set {
+    name  = "server.service.annotations.service\\.beta\\.kubernetes\\.io/azure-pip-name"
+    value = "argocd-server-pip"
+  }
 
-#############################################
-# 3. IP pública del Argo CD Server
-#############################################
-
-# (a) leer el node RG del AKS
-data "azurerm_kubernetes_cluster" "aks" {
-  name                = var.aks_cluster_name
-  resource_group_name = var.resource_group_name
-}
-# (b) leer la Public IP creada por la anotación
-data "azurerm_public_ip" "argocd_lb_ip" {
-  name                = "argocd-server-pip"
-  resource_group_name = data.azurerm_kubernetes_cluster.aks.node_resource_group
-  depends_on          = [helm_release.argocd]     # espera a que exista
-}
-
-output "argocd_public_ip" {
-  description = "Argo CD UI → https://<IP>"
-  value       = data.azurerm_public_ip.argocd_lb_ip.ip_address
+  timeout = 600
 }
