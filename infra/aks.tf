7d50fef8ab64c8fb652793b47ba6e0a503a20eae
4l3j4ndr0 <alejandro.casta3447@gmail.com>
2025-05-09 12:59:58 -0500

    Changes

--- infra/aks.tf
+++ infra/aks.tf
@@ -53,19 +53,19 @@ resource "azurerm_federated_identity_credential" "karpenter_federated_identity" 
 } 
 
 resource "azurerm_role_assignment" "vm_contributor" {  
-  scope                = azurerm_kubernetes_cluster.aks.node_resource_group_id  
+  scope                = azurerm_resource_group.rg.id  
   role_definition_name = "Virtual Machine Contributor"  
   principal_id         = azurerm_user_assigned_identity.karpenter.principal_id  
 }  
 
 resource "azurerm_role_assignment" "network_contributor" {  
-  scope                = azurerm_kubernetes_cluster.aks.node_resource_group_id  
+  scope                = azurerm_resource_group.rg.id  
   role_definition_name = "Network Contributor"  
   principal_id         = azurerm_user_assigned_identity.karpenter.principal_id  
 }  
 
 resource "azurerm_role_assignment" "managed_identity_operator" {  
-  scope                = azurerm_kubernetes_cluster.aks.node_resource_group_id  
+  scope                = azurerm_resource_group.rg.id  
   role_definition_name = "Managed Identity Operator"  
   principal_id         = azurerm_user_assigned_identity.karpenter.principal_id  
 }
