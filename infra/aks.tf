7ce1d4580835c2e6946885a4f1079fd9c0527d0a
ju4nr3v0l <juandavidmarulanda@yahoo.com>
2025-05-09 12:17:32 -0500

    add karpenter managed identity

--- infra/aks.tf
+++ infra/aks.tf
@@ -3,6 +3,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = var.aks_cluster_name
+  oidc_issuer_enabled = true
 
   default_node_pool {
     name            = "agentpool"
@@ -29,4 +30,28 @@ resource "azurerm_kubernetes_cluster" "aks" {
     environment = "poc"
     owner       = "JuanDavid"
   }
+}
+# karpenter identity
+resource "azurerm_user_assigned_identity" "karpenter" {
+  name                = "karpenter-identity"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  tags = {
+    environment = "poc"
+    owner       = "JuanDavid"
+  }
+}
+resource "azurerm_role_assignment" "karpenter_vm_contributor" {
+  scope                = azurerm_resource_group.rg.name != "" ? azurerm_resource_group.rg[0].id : data.azurerm_resource_group.existing_rg[0].id
+  role_definition_name = "Virtual Machine Contributor"
+  principal_id         = azurerm_user_assigned_identity.karpenter.principal_id
+}
+# Crear la credencial federada
+resource "azuread_application_federated_identity_credential" "karpenter" {
+  application_object_id = data.azuread_user_assigned_identity.karpenter.principal_id
+  issuer                = azurerm_kubernetes_cluster.aks.oidc_issuer_url
+  subject               = "system:serviceaccount:karpenter:karpenter"
+  audiences             = ["api://AzureADTokenExchange"]
+  display_name          = ""
 }
