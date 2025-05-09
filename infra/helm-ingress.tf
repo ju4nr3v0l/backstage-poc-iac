fc8e9ae60ebe80020d334fb438fc568b2cf79c9f
ju4nr3v0l <juandavidmarulanda@yahoo.com>
2025-05-09 10:33:21 -0500

    fix ingress version

--- infra/helm-ingress.tf
+++ infra/helm-ingress.tf
@@ -2,7 +2,7 @@ resource "helm_release" "nginx_ingress" {
   name       = "nginx-ingress"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
-  version    = "5.7.0"
+  version    = "4.12.2"
 
   namespace        = "ingress-basic"
   create_namespace = true
