8dd128ef9dff87ab8f5f7aace7ca3dd16683c7e4
ju4nr3v0l <juandavidmarulanda@yahoo.com>
2025-05-09 09:46:58 -0500

    change obtain ip ingress

--- infra/helm-ingress.tf
+++ infra/helm-ingress.tf
@@ -1,5 +1,5 @@
 resource "helm_release" "nginx_ingress" {
-  name       = "nginx-ingress"
+  name       = "nginx-ingress-controller"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "nginx-ingress-controller"
   version    = "5.7.0"
