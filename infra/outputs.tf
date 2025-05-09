6783cfdf36afa2621481471478fc82e9421f3d03
ju4nr3v0l <juandavidmarulanda@yahoo.com>
2025-05-09 09:44:17 -0500

    change obtain ip ingress

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -6,5 +6,6 @@ output "kube_config" {
 
 output "ingress_ip" {
   description = "IP pública del Ingress Controller"
-  value       = helm_release.nginx_ingress.status[0].load_balancer_ingress[0].ip
+  # status.0.load_balancer.0.ingress.0.ip según el esquema del provider k8s
+  value       = data.kubernetes_service.nginx_ingress.status[0].load_balancer[0].ingress[0].ip
 }
