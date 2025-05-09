bfcde8e76b07a7fb3ac0d2bc2ce1f6a8f39cc54c
ju4nr3v0l <juandavidmarulanda@yahoo.com>
2025-05-09 09:32:42 -0500

    change all workflow

--- infra/aks.tf
+++ infra/aks.tf
@@ -5,35 +5,28 @@ resource "azurerm_kubernetes_cluster" "aks" {
   dns_prefix          = var.aks_cluster_name
 
   default_node_pool {
-    name            = "agentpool"
-    node_count      = var.agent_count
-    vm_size         = var.agent_vm_size
-    vnet_subnet_id  = azurerm_subnet.aks.id
+    name           = "agentpool"
+    node_count     = var.agent_count
+    vm_size        = var.agent_vm_size
+    vnet_subnet_id = azurerm_subnet.aks.id
     os_disk_size_gb = 30
-    type            = "VirtualMachineScaleSets"
-    max_pods        = 110
-  }
-
-  network_profile {
-    network_plugin     = "azure"          # Azure CNI
-    network_policy     = "calico"         # Opcional: calico para network policy
-    service_cidr       = "10.0.2.0/24"
-    dns_service_ip     = "10.0.2.10"
-    docker_bridge_cidr = "172.17.0.1/16"
+    type           = "VirtualMachineScaleSets"
+    max_pods       = 110
   }
 
   identity {
     type = "SystemAssigned"
   }
 
-  role_based_access_control {
-    enabled = true
-  }
+  # Habilitar RBAC (antes: role_based_access_control)
+  rbac_enabled = true
 
-  addon_profile {
-    kube_dashboard {
-      enabled = false
-    }
+  network_profile {
+    network_plugin  = "azure"
+    network_policy  = "calico"       # opcional: Calico para políticas de red
+    service_cidr    = "10.0.2.0/24"
+    dns_service_ip  = "10.0.2.10"
+    # docker_bridge_cidr ya no es soportado; Azure asigna automáticamente el bridge
   }
 
   tags = {
