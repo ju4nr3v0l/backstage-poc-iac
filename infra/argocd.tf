fd5cd5b877b4ba0c23b579c89634f1914adfcf98
ju4nr3v0l <juandavidmarulanda@yahoo.com>
2025-05-09 21:45:31 -0500

    add argo manifiest and commend argocd tf

--- infra/argocd.tf
+++ infra/argocd.tf
@@ -1,29 +1,29 @@
-##################################
-# Helm release Argo CD simplificada
-##################################
-resource "helm_release" "argocd" {
-  name             = "argocd"
-  repository       = "https://argoproj.github.io/argo-helm"
-  chart            = "argo-cd"
-  version          = "5.46.6"
-  namespace        = "argocd"
-  create_namespace = true         # Helm crea el NS
-
-  set {
-    name  = "installCRDs"
-    value = "true"
-  }
-
-  # Exponer con LoadBalancer (sin DNS todavía)
-  set {
-    name  = "server.service.type"
-    value = "LoadBalancer"
-  }
-  set {
-    name  = "server.service.annotations.service\\.beta\\.kubernetes\\.io/azure-pip-name"
-    value = "argocd-server-pip"
-  }
-
-  timeout = 1200
-
-}
+# ##################################
+# # Helm release Argo CD simplificada
+# ##################################
+# resource "helm_release" "argocd" {
+#   name             = "argocd"
+#   repository       = "https://argoproj.github.io/argo-helm"
+#   chart            = "argo-cd"
+#   version          = "5.46.6"
+#   namespace        = "argocd"
+#   create_namespace = true         # Helm crea el NS
+#
+#   set {
+#     name  = "installCRDs"
+#     value = "true"
+#   }
+#
+#   # Exponer con LoadBalancer (sin DNS todavía)
+#   set {
+#     name  = "server.service.type"
+#     value = "LoadBalancer"
+#   }
+#   set {
+#     name  = "server.service.annotations.service\\.beta\\.kubernetes\\.io/azure-pip-name"
+#     value = "argocd-server-pip"
+#   }
+#
+#   timeout = 1200
+#
+# }
