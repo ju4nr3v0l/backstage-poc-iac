d5ddf4b6f19d4dec6a69575bf10bf24a9d12013f
ju4nr3v0l <juandavidmarulanda@yahoo.com>
2025-05-13 10:31:43 -0500

    change resource group name

--- infra/ingress_data.tf
+++ infra/ingress_data.tf
@@ -1,11 +1,11 @@
 # 1) Obtenemos el nombre del node_resource_group generado por AKS
 data "azurerm_kubernetes_cluster" "aks" {
-  name                = var.aks_cluster_name
-  resource_group_name = var.resource_group_name
+  name                = azurerm_kubernetes_cluster.aks.name
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 # 2) Leemos el Public IP que Azure ya creó, con nombre fijo
 data "azurerm_public_ip" "nginx_ingress" {
   name                = "nginx-ingress-pip"
-  resource_group_name = var.resource_group_name
+  resource_group_name = data.azurerm_kubernetes_cluster.aks.node_resource_group
 }
