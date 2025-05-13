22c466c29134f61d8b7c1363baf85ca1cc257783
ju4nr3v0l <juandavidmarulanda@yahoo.com>
2025-05-13 10:44:47 -0500

    change vnet subred directions

--- infra/network.tf
+++ infra/network.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.0.0.0/16", "10.1.0.0/16"]
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 }
